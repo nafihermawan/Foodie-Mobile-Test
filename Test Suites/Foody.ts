70f5f297dc171354c611d8830d5c405d89600f54
nafihermawan <nafihermawan@gmail.com>
2021-08-13 20:08:21 +0700

    initialization commit

---
+++ Test Suites/Foody.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Foody</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>253f0673-6fbc-4763-b214-4177c2e5d656</testSuiteGuid>
+   <testCaseLink>
+      <guid>046ce0a6-90d2-4a5d-9764-fccad7600c09</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC001_StartApp</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>690645a2-31e8-4085-9993-f33e53dfdcd9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC002_LoginFB</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4abd37e7-e991-4ed0-a999-85d6592289cb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC003_MencariMakanan</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
